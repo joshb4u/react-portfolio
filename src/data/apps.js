ced2361b52e56512d3b22d0236f865e75885248b
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-07-05 13:33:56 -0400

    Added data.js,tech.js & images

---
+++ src/data/apps.js
@@ -0,0 +1,48 @@
+const apps = [
+  {
+    name: "Eat-Da-Burger",
+    description:
+      "A burger logger webpage created with MySQL, Node, Express, Handlebars and ORM",
+    image: require("../img/burger.png"),
+    imagewebp: require("../img/burger.webp"),
+    links: {
+      githubUrl: "https://github.com/joshb4u/Hw13",
+      demoUrl: "https://whispering-everglades-09702.herokuapp.com/",
+    },
+  },
+  {
+    name: "Weather Dashboard",
+    description:
+      "A Weather Dashboard for displaying weather information for any city.",
+    image: require("../img/weather.png"),
+    imagewebp: require("../img/weather.webp"),
+    links: {
+      githubUrl: "https://github.com/joshb4u/Hw6",
+      demoUrl: "https://joshb4u.github.io/Hw6/",
+    },
+  },
+  {
+    name: "Master Home Cook",
+    description:
+      "A webpage for viewing food recipes for cooking enthusiasts.",
+    image: require("../img/masterhomecook.png"),
+    imagewebp: require("../img/masterhomecook.webp"),
+    links: {
+      githubUrl: "https://github.com/maxguojiaqi/Master-Home-Cook",
+      demoUrl: "https://maxguojiaqi.github.io/Master-Home-Cook/",
+    },
+  },
+  {
+    name: "Gotchitama",
+    description:
+      "A web browser game, based on Tamagotchi and Pokemon, where users can interact with their pet.",
+    image: require("../img/gotchitama.png"),
+    imagewebp: require("../img/gotchitama.webp"),
+    links: {
+      githubUrl: "https://github.com/sandyboon/gotchitama",
+      demoUrl: "https://sleepy-badlands-73130.herokuapp.com/",
+    },
+  },
+];
+
+export default apps;
---
+++ src/data/tech.js
@@ -0,0 +1,61 @@
+
+const tech = [
+    {
+        "name": "Bootstrap4",
+        "image": require("../img/icons/bootstrap.png"),
+    },
+    {
+        "name": "CSS3",
+        "image": require("../img/icons/css3.png"),
+    },
+    {
+        "name": "Express.js",
+        "image": require("../img/icons/express.png"),
+    },
+    {
+        "name": "Git",
+        "image": require("../img/icons/git.png"),
+    },
+    {
+        "name": "Handlebars",
+        "image": require("../img/icons/handlebars-js.png"),
+    },
+    {
+        "name": "HTML5",
+        "image": require("../img/icons/html5.png"),
+    },
+    {
+        "name": "Javascript",
+        "image": require("../img/icons/js.png"),
+    },
+    {
+        "name": "jQuery",
+        "image": require("../img/icons/jquery.png"),
+    },
+    {
+        "name": "MongoDB",
+        "image": require("../img/icons/mongodb.png"),
+    },
+    {
+        "name": "Mongoose",
+        "image": require("../img/icons/mongoose.png"),
+    },
+    {
+        "name": "MySQL",
+        "image": require("../img/icons/mysql.png"),
+    },
+    {
+        "name": "Node.js",
+        "image": require("../img/icons/node.png"),
+    },
+    {
+        "name": "React",
+        "image": require("../img/icons/react.png"),
+    },
+    {
+        "name":"Sequelize",
+        "image": require("../img/icons/sequelize.png"),
+    },
+]
+
+export default tech;
