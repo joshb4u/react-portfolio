09a1421c4b2ccc9fb1f1792830a4c5245faf812a
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-07-05 14:00:27 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AboutMe from "./Pages/AboutMe/AboutMe";
+import ContactForm from "./Pages/ContactMe/ContactForm";
+import Footer from "./components/Footer/footer";
+import Greeting from "./Pages/Greeting/Greeting";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Sidebar2 from "./components/sidebar2/sidebar2"
+import "./Pages/style.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Sidebar2/>
+    <Greeting/>
+    <AboutMe/>
+    <Portfolio/>
+    <ContactForm/>
+    <Footer/>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
