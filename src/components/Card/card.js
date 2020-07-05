9bd49d3a7203436080589113fa946e33d66cc13d
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-07-05 13:19:47 -0400

    added card component with styling, deleted unwanted files

---
+++ src/components/Card/card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Card(props) {
+    const app = props.app;
+    return(
+            <div className="card">
+                <img src= {app.image} className="card-img-top" alt={app.name} />
+                    <div className="card-body">
+                        <h5 className="card-title">{app.name}</h5>
+                        <p className="card-text">{app.description}</p>
+                        <button type="button" className="btn btn-light btn-sm"><a href={app.links.githubUrl}>GITHUB</a></button>
+                        <button type="button" className="btn btn-light btn-sm"><a href={app.links.demoUrl}>LIVE</a></button>
+                    </div>
+            </div>
+    )
+}
+
+export default Card;
