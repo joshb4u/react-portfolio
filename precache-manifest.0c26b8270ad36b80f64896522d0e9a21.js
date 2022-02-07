0717f5b5dcdf2c4c57388557ebec6dff54d496fe
Josh Babu George <joshbabugeorge@Joshs-MacBook-Air.local>
2022-02-07 12:05:37 -0500

    Updates

---
+++ precache-manifest.0c26b8270ad36b80f64896522d0e9a21.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b1e2b339fbb92b246750ceb7b86549",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a294703563b32505584e",
+    "url": "/react-portfolio/static/css/main.856f5802.chunk.css"
+  },
+  {
+    "revision": "70bb028f7c353ceee293",
+    "url": "/react-portfolio/static/js/2.90057ff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.90057ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a294703563b32505584e",
+    "url": "/react-portfolio/static/js/main.2bc4b05f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "bc82eb5bfd8ad3d056d2d7fbb5473503",
+    "url": "/react-portfolio/static/media/background.bc82eb5b.jpg"
+  },
+  {
+    "revision": "1454758f25757872887bd7825dfa6946",
+    "url": "/react-portfolio/static/media/burger.1454758f.png"
+  },
+  {
+    "revision": "1454758f25757872887bd7825dfa6946",
+    "url": "/react-portfolio/static/media/burger.1454758f.webp"
+  },
+  {
+    "revision": "dc8d20ceec53a6559196b09b99813ddc",
+    "url": "/react-portfolio/static/media/googlebooks.dc8d20ce.png"
+  },
+  {
+    "revision": "dc8d20ceec53a6559196b09b99813ddc",
+    "url": "/react-portfolio/static/media/googlebooks.dc8d20ce.webp"
+  },
+  {
+    "revision": "3798dc5fd507e70be42375a531761a47",
+    "url": "/react-portfolio/static/media/gotchitama.3798dc5f.png"
+  },
+  {
+    "revision": "3798dc5fd507e70be42375a531761a47",
+    "url": "/react-portfolio/static/media/gotchitama.3798dc5f.webp"
+  },
+  {
+    "revision": "6cd1ab9a1d42018ed4b226acea9932a1",
+    "url": "/react-portfolio/static/media/masterhomecook.6cd1ab9a.png"
+  },
+  {
+    "revision": "6cd1ab9a1d42018ed4b226acea9932a1",
+    "url": "/react-portfolio/static/media/masterhomecook.6cd1ab9a.webp"
+  },
+  {
+    "revision": "ac950fa105d666c0a45ddfc6b9d728df",
+    "url": "/react-portfolio/static/media/portfoliopic.ac950fa1.png"
+  }
+]);
